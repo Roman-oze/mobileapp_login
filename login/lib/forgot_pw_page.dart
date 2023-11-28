53806131b66790d8d89a43189f72a7974a6e00fd
Roman-oze <rumuislam202@gmail.com>
2023-11-28 22:21:06 +0600

    comment methid and onpressed button

--- login/lib/forgot_pw_page.dart
+++ login/lib/forgot_pw_page.dart
@@ -1,5 +1,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:_firebase/cloud_firestore.dart';
+
 import 'package:flutter/material.dart';
 import 'package:login/reusable_widget/reusable_widget.dart';
 
@@ -12,29 +14,28 @@ class ForgotPasswordPage extends StatefulWidget {
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   TextEditingController _emailTextController = TextEditingController();
-
   @override
-  void dispose(){
-    _emailTextController.dispose();
-    super.dispose();
-    Future passwordReset () async {
-     try{
-       await FirebaseAuth.instance.SendPasswordResetEmail(email:_emailTextController.text.trim());
-       showDialog(context: context, builder: context){
-           return AlertDialog(
-             content: Text('Reset password check your email'),
-           );
-         }
-     }  FirebaseAuthException catch(e){
-     print(e);
-     // showDialog(context: context, builder: context){
-     //   return AlertDialog(
-     //     content: Text(e.message.toString()),
-     //   );
-     // }
-     }
-    }
-  }
+  // void dispose(){
+  //   _emailTextController.dispose();
+  //   super.dispose();
+  //   Future passwordReset () async {
+  //    try{
+  //      await FirebaseAuth.instance.SendPasswordResetEmail(email:_emailTextController.text.trim());
+  //      showDialog(context: context, builder: context){
+  //          return AlertDialog(
+  //            content: Text('Reset password check your email'),
+  //          );
+  //        }
+  //    }  FirebaseAuthException catch(e){
+  //    print(e);
+  //    showDialog(context: context, builder: context){
+  //      return AlertDialog(
+  //        content: Text(e.message.toString()),
+  //      );
+  //    }
+  //    }
+  //   }
+  // }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -73,11 +74,11 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
                  ),
               ),
      SizedBox(height: 20,),
-          MaterialButton(onPressed: passwordReset,
-            child: Text('Reset Password'),
-            color: Colors.blueAccent,
-
-          )
+          // MaterialButton(onPressed: passwordReset,
+          //   child: Text('Reset Password'),
+          //   color: Colors.blueAccent,
+          //
+          // )
         ],
       ),
     );
